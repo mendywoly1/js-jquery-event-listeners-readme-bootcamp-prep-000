ad771e9a73199187a8a303edcc7420e4213eafcd
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-29 20:59:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,19 @@
 $(document).ready(function(){
 
 // call functions here
+getIt()
+frameIt()
 
 });
+
+function getIt() {
+  $('p').on('click', function(key){
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  });
+}
